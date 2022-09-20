e723e4c8794308a2416b12f0300b35f3bb732068
oguzhankarakoc <68776443+oguzhankarakoc@users.noreply.github.com>
2022-09-20 12:32:03 +0300

    Yenilikller

--- NLayer.Core/Repositories/ICategoryRepository.cs
+++ NLayer.Core/Repositories/ICategoryRepository.cs
@@ -9,6 +9,6 @@ namespace NLayer.Core.Repositories
 {
     public interface ICategoryRepository:IGenericRepository<Category>
     {
-        Task<Category> GetSingleCategoryByIdWithProductsAsync(int id);
+        Task<Category> GetSingleCategoryByIdWithProductsAsync(int categoryId);
     }
 }
