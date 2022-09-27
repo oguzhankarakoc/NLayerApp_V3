81e90798126765cf9f7be2514c42db5c416b6038
oguzhankarakoc <68776443+oguzhankarakoc@users.noreply.github.com>
2022-09-27 17:10:12 +0300

    Not Found Filter

--- NLayer.API/Controllers/ProductsController.cs
+++ NLayer.API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using NLayer.API.Filters;
 using NLayer.Core.DTOs;
 using NLayer.Core.Models;
 using NLayer.Core.Services;
@@ -43,6 +44,8 @@ namespace NLayer.API.Controllers
             return CreateActionResult (CustomResponseDto<List<ProductDto>>.Success(200, productsDtos));
 
         }
+
+        [ServiceFilter(typeof(NotFoundFilter<Product>))]//Bu filtre istenirse direk Remove metoduna da kullanılabilir, sadece metodun üzerine eklenmesi yeterlidir!
         //GET /api/products/2
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -28,6 +28,8 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
     options.SuppressModelStateInvalidFilter = true;
 });
 
+builder.Services.AddScoped(typeof(NotFoundFilter<>));
+
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
