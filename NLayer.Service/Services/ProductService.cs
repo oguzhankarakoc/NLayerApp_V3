21e720396f8d805053ab9e44adfe3f5de0d803c1
oguzhankarakoc <68776443+oguzhankarakoc@users.noreply.github.com>
2022-09-30 13:16:46 +0300

    MVC Controller

--- NLayer.Service/Services/ProductService.cs
+++ NLayer.Service/Services/ProductService.cs
@@ -12,22 +12,22 @@ using System.Threading.Tasks;
 
 namespace NLayer.Service.Services
 {
-    public class ProductServiceWithNoCaching : Service<Product>, IProductService
+    public class ProductService : Service<Product>, IProductService
     {
         private readonly IProductRepository _productReposiyory;
         private readonly IMapper _mapper;
-        public ProductServiceWithNoCaching(IGenericRepository<Product> repository, IUnitOfWork unitOfWork,IMapper mapper,IProductRepository productRepository) : base(repository, unitOfWork)
+        public ProductService(IGenericRepository<Product> repository, IUnitOfWork unitOfWork,IMapper mapper,IProductRepository productRepository) : base(repository, unitOfWork)
         {
             _mapper = mapper;
             _productReposiyory = productRepository;
         }
 
-        public async Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetProductsWithCategory()
+        public async Task<List<ProductWithCategoryDto>> GetProductsWithCategory()
         {
             var products = await _productReposiyory.GetProductsWithCategory();
 
             var productsDto= _mapper.Map<List<ProductWithCategoryDto>>(products); 
-            return CustomResponseDto<List<ProductWithCategoryDto>>.Success(200,productsDto);
+            return productsDto;
 
         }
     }
